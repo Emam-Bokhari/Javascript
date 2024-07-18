9864e1217e893725dc7f69cf4b68b7aacf5f6e2b
Moshfiqur Rahman <studentemam@gmail.com>
2024-07-18 20:26:32 +0600

    string

---
+++ string.js
@@ -0,0 +1,13 @@
+let a = "This is Moshfiqur Rahman";
+let b = "This is Emam Bokhari";
+let c = `This is Bokhari`;
+let d = 24;
+let e=24;
+let f=24;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(String(d));
+console.log(e.toString());
+console.log(f.toLocaleString())
