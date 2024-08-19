2c72b663701dc1f643323303912dfba6cda2970f
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-19 19:46:44 +0600

    do while loop

---
+++ doWhileLoop.js
@@ -0,0 +1,17 @@
+// ex:01
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+// ex:02
+let j = 0;
+do {
+  if (j % 2 == 0) {
+    console.log("Even Number", j);
+  } else {
+    console.log("Odd Number", j);
+  }
+  j++;
+} while (j <= 100);
