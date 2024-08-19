2ffa60ded9d2a977a9a5709d10a13296c359e94a
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-19 19:41:44 +0600

    git push

---
+++ whileLoop.js
@@ -0,0 +1,17 @@
+// ex:01
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// ex:02
+let j = 0;
+while (j <= 100) {
+  if (j % 2 == 0) {
+    console.log("Even Number", j);
+  } else {
+    console.log("Odd Number", j);
+  }
+  j++;
+}
