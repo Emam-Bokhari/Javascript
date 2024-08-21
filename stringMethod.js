1c7b162a1f2bcde568abd60ff881a8dffc4ffdc9
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-21 19:49:03 +0600

    concat,substring method

---
+++ stringMethod.js
@@ -0,0 +1,12 @@
+// concat method
+// Ex:01
+// concat method ar madhome amra akti string ar sathe arekti string k jog korte pari
+const a = "Hello";
+const b = "World!";
+console.log(a.concat(b));
+
+// substring method
+// Ex:01
+// string theke nirdisto onghso ber kore anar jonno amara substing method use korte pari
+const c="This is Apple";
+console.log(c.substring(0,5));
