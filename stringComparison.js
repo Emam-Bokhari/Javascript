fb0d464a7deaf288bab7b7eb8726f73e8a5e16e3
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-21 19:45:27 +0600

    string comparison

---
+++ stringComparison.js
@@ -0,0 +1,24 @@
+// Ex:01
+const x = "20";
+const y = 30;
+if (x > y) {
+  console.log("X is greater than Y", x);
+} else {
+  console.log("Y is greater than X", y);
+}
+
+// Ex:02
+const a = "a";
+const b = "B";
+if (a > b) {
+  console.log("a is greater than B");
+} else {
+  console.log("B is greater than a");
+}
+
+// Ex:03
+const z="z";
+const Z="Z";
+if(z>Z){
+    console.log(true);
+}
