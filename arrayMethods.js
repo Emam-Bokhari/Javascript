b569c7d36f930c7fa8a98fdd514cae415b75b4e8
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-23 20:07:03 +0600

    array reverse method

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,34 +1,39 @@
 // Ex:01
 // insert and delete method
 // pop method
-const a=[1,2,3,4,5,6];
+const a = [1, 2, 3, 4, 5, 6];
 a.pop();
 console.log(a);
 
 // Ex:02
 // push method
-const b=[1,2,3,4,5];
-b.push(12,111);
+const b = [1, 2, 3, 4, 5];
+b.push(12, 111);
 console.log(b);
 
 // Ex:03
 // shift method
-const c=[1,2,3,4,5];
+const c = [1, 2, 3, 4, 5];
 c.shift();
 console.log(c);
 
 // Ex:04
 // unShift method
-const d=[1,2,3,4,5];
+const d = [1, 2, 3, 4, 5];
 d.unshift(0);
 console.log(d);
 
 // Ex:05
-const e=[1,2,3,4,5];
-e.splice(0,0,55);
+const e = [1, 2, 3, 4, 5];
+e.splice(0, 0, 55);
 console.log(e); //inseet new data
 
 // Ex:06
-const f=[1,2,3,4,5];
-f.splice(0,2);
-console.log(f);
+const f = [1, 2, 3, 4, 5];
+f.splice(0, 2);
+console.log(f); // delete data
+
+// Ex:07
+const g=[1,2,3,4,5];
+g.reverse();
+console.log(g);
