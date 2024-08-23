f1a9794db5e84c9a6e4e783bd98b33abd63b9ecd
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-23 19:33:56 +0600

    array declaration

---
+++ arrayDeclaration.js
@@ -0,0 +1,16 @@
+// Ex:01
+const a=["Apple","Mango","Banana"];
+console.log(a[0]);
+
+// Ex:02
+const b=["Apple","Mango","Banana"];
+b[3]="Jackfruit";
+console.log(b);
+
+// Ex:03
+const c=["A","B","C","D"];
+console.log(c.length)
+
+// Ex:04
+let d=Array(0,1,2);
+console.log(d)
