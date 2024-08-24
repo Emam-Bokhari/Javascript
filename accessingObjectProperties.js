34940ee380f220d96561bd5d9f139929614e7131
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-24 23:39:24 +0600

    accessing object properties

---
+++ accessingObjectProperties.js
@@ -0,0 +1,14 @@
+const x = { id: 1, name: "Emam Bokhari", address: "Brahmanbaria" };
+console.log(x.name); // dot notation
+console.log(x["name"]); // array notation
+
+// Ex:01 addition two object values
+const a = { numberOne: 1 };
+const b = { numberTwo: 2 };
+console.log(a.numberOne + b.numberTwo);
+
+const obj = { x: 10, y: 11, z: 12 };
+const prop="d";
+obj["c"] = 101;
+obj.prop=102;
+console.log(obj);
