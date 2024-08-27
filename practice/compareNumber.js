e5330badcb3559b769aa9f73eb12f41e9ef36815
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-27 21:06:55 +0600

    compare two numbers

---
+++ practice/compareNumber.js
@@ -0,0 +1,17 @@
+const x=20;
+const y=30;
+
+if(x>y){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+const a=Number("30")
+const b=30
+
+if(a===b){
+    console.log(true);
+}else{
+    console.log(false);
+}
