0bdcb526b169e760d969acfab69d61684dacbcae
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-27 21:39:12 +0600

    array basic method

---
+++ practice/arrayMethod.js
@@ -0,0 +1,24 @@
+const a=[1,2,3,4,5,6];
+const b=[1,2,3,4,5,6];
+const c=[1,2,3,4,5,6];
+const d=[1,2,3,4,5,6];
+const e=[1,2,3,4,5,6];
+const f=[1,2,3,4,5,6];
+
+a.push(7);
+console.log(a)
+
+b.pop();
+console.log(b);
+
+c.shift();
+console.log(c);
+
+d.unshift(7);
+console.log(d);
+
+e.splice(0,0,11);
+console.log(e);
+
+f.splice(4,1);
+console.log(f);
