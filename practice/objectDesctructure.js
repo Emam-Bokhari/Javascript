fe494ca06c2e335b5c8a46b2b081a878aec68c97
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-28 19:47:07 +0600

    object desctructure

---
+++ practice/objectDesctructure.js
@@ -0,0 +1,14 @@
+const marks = {
+  math: 33,
+  higherMath: 90,
+  biology: 85,
+  physics: 55,
+};
+
+const { math } = marks;
+const { higherMath, biology, physics } = marks;
+
+console.log(math);
+console.log(math, higherMath, biology, physics);
+console.log(marks);
+console.log(marks.math);
