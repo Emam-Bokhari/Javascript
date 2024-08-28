e72a0508e1e4e912cc2e862a6ae9d9d67570d047
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-28 20:01:34 +0600

    shallow copy

---
+++ practice/shallowCopy.js
@@ -0,0 +1,31 @@
+const bokhariDetails = {
+  name: "Emam Bokhari",
+  age: 25,
+  address: {
+    district: "Brahmanbaria",
+    contact: {
+      number: "+880 131-57-73 424",
+    },
+  },
+};
+
+console.log(bokhariDetails);
+console.log(bokhariDetails.address.contact);
+
+// shalow copy
+const newObject = {
+  ...bokhariDetails, // ai code ti bokhariDetails ar shallow copy toiri kore
+  age:24,
+  address: {
+    // address ar shallow copy toiri kore
+    ...bokhariDetails.address,
+    contact: {
+      // contact ar shallow copy toiri kore
+      ...bokhariDetails.address.contact,
+      number:"01315773424",
+      phone: "Not Allow!",
+    },
+  },
+};
+
+console.log(newObject,bokhariDetails);
