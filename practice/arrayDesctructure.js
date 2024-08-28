e81046775729b7122bc7c4ec1a11971126626de7
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-28 19:43:31 +0600

    array destructure

---
+++ practice/arrayDesctructure.js
@@ -0,0 +1,9 @@
+const x = [1, 2, 3, 4, 5];
+
+const [numberOne, numberTwo, numberThree, numberFour, numberFive] = x;
+
+console.log(numberOne);
+console.log(numberOne,numberTwo,numberThree,numberFour,numberFive);
+
+console.log(x);
+
