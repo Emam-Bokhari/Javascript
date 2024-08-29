c3db68a7bacea25f7f6cb66221063f8f3cefcdf9
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-29 19:58:31 +0600

    function invokation/invoking

---
+++ invoking.js
@@ -0,0 +1,10 @@
+function add(){
+    const x=10;
+    const y=20;
+    console.log(x+y);
+}
+add()
+
+for(let i=0;i<10;i++){
+    add()
+}
--- practice/shallowCopy.js
+++ practice/shallowCopy.js
@@ -15,17 +15,17 @@ console.log(bokhariDetails.address.contact);
 // shalow copy
 const newObject = {
   ...bokhariDetails, // ai code ti bokhariDetails ar shallow copy toiri kore
-  age:24,
+  age: 24,
   address: {
     // address ar shallow copy toiri kore
     ...bokhariDetails.address,
     contact: {
       // contact ar shallow copy toiri kore
       ...bokhariDetails.address.contact,
-      number:"01315773424",
+      number: "01315773424",
       phone: "Not Allow!",
     },
   },
 };
 
-console.log(newObject,bokhariDetails);
+console.log(newObject, bokhariDetails);
