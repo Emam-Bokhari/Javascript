a2851ebb70f53ffac4a0b45eee3f7c5b1b1dfaaa
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-29 20:14:13 +0600

    function expression

---
+++ functionExpression.js
@@ -0,0 +1,12 @@
+const addition = function (x, y, z) {
+  return (result = x + y + z);
+};
+const add=addition(2,2,2);
+console.log(add)
+console.log(addition(2,2,2));
+
+// ex:02
+setTimeout(()=>{
+    console.log("Hello World!");
+},1000);
+
