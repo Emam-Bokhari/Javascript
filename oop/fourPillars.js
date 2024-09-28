288f70217a34cdf3cc4e4bbb09b2e29d236cbea8
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-28 06:02:38 +0600

    docs(oop): add description of the four pillars of oop

---
+++ oop/fourPillars.js
@@ -0,0 +1,8 @@
+// four pillars of oop
+
+/* 
+1. Abstraction - functionality hide kora
+2. Encapsulation - capsulated thaka
+3. Inheritance - onner boishisto nijer maje dharon kora
+4. Polymarphism - bohoropi/onk rop
+*/
