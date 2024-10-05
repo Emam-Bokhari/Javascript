6e387b0dc5d518d59292b7ed01d797d114002bcb
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-05 07:51:46 +0600

    feat(constructor, and factory pattern): add constructor and factory pattern with examples

--- oop/constructorPattern/script.js
+++ oop/constructorPattern/script.js
@@ -1,5 +1,5 @@
 // constructor pattern
-// constructor likhar somai sobsomai e 1st letter boro hater likhte hobe. 
+// constructor likhar somai sobsomai e 1st letter boro hater likhte hobe.
 const CreateRectangle = function (width, height) {
   this.width = width;
   this.height = height;
@@ -11,6 +11,19 @@ const CreateRectangle = function (width, height) {
 
 const rect = new CreateRectangle(3, 3);
 rect.calculateArea();
+
+// constructor pattern revise
+const CalculateRectangle = function (width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.calculateArea = function () {
+    console.log(width * height);
+  };
+};
+
+const rect3 = new CalculateRectangle(7, 9);
+rect3.calculateArea();
 
 // factory pattern
 const rectangle = function (width, height) {
@@ -24,5 +37,20 @@ const rectangle = function (width, height) {
   };
 };
 
-const rect2=rectangle(3,3);
-rect2.calculateArea()
+const rect2 = rectangle(3, 3);
+rect2.calculateArea();
+
+// factory pattern revise
+const rectangle2 = function (width, height) {
+  return {
+    width: width,
+    height: height,
+
+    calculateArea: function () {
+      console.log(this.width * this.height);
+    },
+  };
+};
+
+const rect4 = rectangle2(3, 5);
+rect4.calculateArea();
