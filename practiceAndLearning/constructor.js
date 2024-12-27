5441b89663a6e626a784b7a816a3e9e95a45df70
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-27 22:54:34 +0600

    feat: added Car calss with displayInfo method

--- practiceAndLearning/constructor.js
+++ practiceAndLearning/constructor.js
@@ -11,3 +11,17 @@ class Person {
 
 const bokhari = new Person("Emam Bokhari", 24);
 bokhari.introduce();
+
+class Car {
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+
+  displayInfo() {
+    console.log(`This car is a ${this.brand} ${this.model}`);
+  }
+}
+
+const toyota = new Car("Toyota", "M-0009");
+toyota.displayInfo();
