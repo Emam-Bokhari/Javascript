336bbeebc9a2dce26bd922837cd6cb947f451e77
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-27 23:14:15 +0600

    feat: implement private fields

--- practiceAndLearning/constructor.js
+++ practiceAndLearning/constructor.js
@@ -63,3 +63,55 @@ class Rectangle {
 
 const rect1 = new Rectangle(5, 10);
 rect1.describe();
+
+class Vehicle {
+  constructor(type) {
+    this.type = type;
+  }
+
+  describe() {
+    console.log(`This is a ${this.type}`);
+  }
+}
+
+class Bike extends Vehicle {
+  constructor(type, brand) {
+    super(type); // parent constructor call
+    this.brand = brand;
+  }
+
+  displayInfo() {
+    console.log(`This is a ${this.brand} ${this.type}.`);
+  }
+}
+
+// create bike object
+const bike = new Bike("Motorbike", "Yamha");
+
+bike.describe();
+bike.displayInfo();
+
+// private class fields
+class PersonTwo {
+  #name; // private field
+
+  constructor(name, age) {
+    this.#name = name;
+    this.age = age;
+  }
+
+  // public method
+  getName() {
+    // return this.#name, this.age;
+    return {
+      name: this.#name,
+      age: this.age,
+    };
+  }
+}
+
+const person1 = new PersonTwo("Moshfiqur Rahman", 24);
+
+console.log(person1.getName());
+console.log(person1.age);
+console.log(person1.name);
