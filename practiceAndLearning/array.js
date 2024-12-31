67805b4a2a91b414e6228eaae992ab17b624fc8e
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-31 11:59:48 +0600

    feat: add array map method

--- practiceAndLearning/array.js
+++ practiceAndLearning/array.js
@@ -1,9 +1,30 @@
-// array cration
-const arr = [1, 2, 3, 4];
-console.log(arr);
+{
+  // array cration
+  const arr = [1, 2, 3, 4];
+  console.log(arr);
 
-const arr2 = new Array(5);
-console.log(arr2);
+  const arr2 = new Array(5);
+  console.log(arr2);
 
-const arr3 = Array.from("*");
-console.log(arr3);
+  const arr3 = Array.from("*");
+  console.log(arr3);
+
+  const numbers = [10, 21, 35, 41, 56];
+  console.log(numbers[1]);
+}
+// array methods
+
+/**
+ * map
+ * filter
+ * reduce
+ * forEach
+ * slice
+ * splice
+ * sort
+ */
+
+// map
+const numbers = [1, 2, 3];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled);
